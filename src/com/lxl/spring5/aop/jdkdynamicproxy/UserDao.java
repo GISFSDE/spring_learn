bc3b54094c13153aecf1b85578c7427a47f14be1
MrDemonlxl <714416426@qq.com>
2021-12-24 18:56:46 +0800

    update

---
+++ src/com/lxl/spring5/aop/jdkdynamicproxy/UserDao.java
@@ -0,0 +1,8 @@
+package com.lxl.spring5.aop.jdkdynamicproxy;
+
+public interface UserDao {
+    public int add(int a, int b);
+
+    public String update(String id);
+
+}
---
+++ src/com/lxl/spring5/aop/jdkdynamicproxy/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.lxl.spring5.aop.jdkdynamicproxy;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public String update(String id) {
+        return id;
+    }
+}
