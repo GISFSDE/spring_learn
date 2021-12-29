38dda9dd8d1af58354485ed8dad52cc6fd8eb1a1
MrDemonlxl <714416426@qq.com>
2021-12-29 21:12:08 +0800

    Log4j  spring5

--- src/com/lxl/spring5/transactionl/test/transactionTest.java
+++ src/com/lxl/spring5/transactionl/test/transactionTest.java
@@ -3,9 +3,13 @@ package com.lxl.spring5.transactionl.test;
 import com.lxl.spring5.transactionl.TxConfig;
 import com.lxl.spring5.transactionl.service.UserService;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.security.PrivateKey;
 
 public class transactionTest {
     //半注解方式测试
@@ -36,5 +40,11 @@ public class transactionTest {
         UserService userService = context.getBean("userService", UserService.class);
         userService.accountMoney();
 
+    }
+
+    @Test
+    public void  manualLogTest(){
+        final Logger log = LoggerFactory.getLogger(transactionTest.class);
+        log.warn("hello log4j==============");
     }
 }
